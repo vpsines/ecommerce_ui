05bc421cf047acc5e6c2c7174637b9b079852800
sasip6028@gmail.com <--unset>
2023-01-12 11:50:42 +0530

    test 6

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class MyApp extends StatelessWidget {
         // Notice that the counter didn't reset back to zero; the application
         // is not restarted.
       ),
-      home:  SizedBox (),
+      home:  Container (),
     );
   }
 }
